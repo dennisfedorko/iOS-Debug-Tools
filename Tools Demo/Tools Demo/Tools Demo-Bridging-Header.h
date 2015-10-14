18d8164366bf785c3a62e5ce2c36fba1518d92e5
Dennis <dennis.fedorko@gmail.com>
2015-10-14 02:07:37 -0400

    Add support for messages, screenshots, and tools. Removed dependency on Alamofire, used NSURLSession instead

---
+++ Tools Demo/Tools Demo/Tools Demo-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "FBTweak.h"
+#import "FBTweakStore.h"
+#import "FBTweakCategory.h"
+#import "FBTweakCollection.h"
+#import "FBTweakViewController.h"
+#import "FBTweakShakeWindow.h"
---
+++ Tools Demo/Tools Demo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Tools Demo
+//
+//  Created by Dennis Fedorko on 10/4/15.
+//  Copyright © 2015 Dennis Fedorko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.whiteColor()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
